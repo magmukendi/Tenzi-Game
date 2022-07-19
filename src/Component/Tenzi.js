2f5d9bcf140ae6733663ff14c8e5e706591bfeae
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-07-19 21:54:06 +0900

    Tenzi game first commit

---
+++ src/Component/Tenzi.js
@@ -0,0 +1,149 @@
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
+
+
+function Dice(props) {
+  return (
+    <div
+      onClick={props.toggle}
+      style={{ backgroundColor: props.isHeld ? "#59E391" : "white" }}
+      className="die-face"
+    >
+      <h1 className="die-number">{props.value}</h1>
+    </div>
+  );
+}
+function Tenzi() {
+
+  const [dice, setDice] = useState(createRandNumber());
+  const [tenzies, setTenzies] = useState(false);
+  const [timer, setTimer] = useState(0);
+  const [timeDisplay, setTimeDisplay] = useState(true);
+  const [bestTime, setBestTime] = useState(JSON.parse(localStorage.getItem("bestTime")) || []);
+  const [totalRoll, setTotalRoll] = useState(
+    JSON.parse(localStorage.getItem("numberOfRolls")) || []
+  );
+  const [roll, setRoll] = useState(0)
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [dice]);
+
+  ///Generate new die
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    };
+  }
+  useEffect(() => {
+    localStorage.setItem("bestTime", JSON.stringify(bestTime));
+    localStorage.setItem("numberOfRolls", JSON.stringify(totalRoll))
+  }, [totalRoll,bestTime]);
+
+
+  useEffect(() => {
+    setTimeout(function () {
+      setTimer((prev) => {
+       return tenzies ? Number(prev) : Number(prev) + 1
+    });
+    }, 1000);
+  }, [timer]);
+
+ 
+
+  function createRandNumber() {
+    const newArr = [];
+
+    for (let i = 0; i < 10; i++) {
+      newArr.push(generateNewDie());
+    }
+    return newArr;
+  }
+  function rollDice() {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+    setRoll(prevRoll => Number(prevRoll) + 1)
+  }
+  function newGame() {
+    tenzies && setBestTime((prev) => [...prev, Number(timer)]);
+    setDice(createRandNumber());
+    setTenzies(false);
+    setTimer(0);
+    tenzies && setTotalRoll(prev => [...prev, Number(roll)] )
+    setRoll(0)
+  }
+
+  function hold(id) {
+    setDice((prevDice) => {
+      const d = prevDice.map((item) =>
+        item.id === id ? { ...item, isHeld: !item.isHeld } : item
+      );
+      return d;
+    });
+  }
+  const diceElements = dice.map((die) => (
+    <Dice
+      toggle={() => hold(die.id)}
+      value={die.value}
+      key={die.id}
+      isHeld={die.isHeld}
+    />
+  ));
+
+  const bestTimes = bestTime.reverse().map(t =>(
+    <li>{t} sec.</li>
+  ))
+
+
+  const bestRolls = totalRoll.reverse().map(t =>(
+    <li>{t} {t>0?"rolls":roll}</li>
+  ))
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <div className="timer-container">
+        {timeDisplay && <h1 className="timer">{timer}</h1>}
+      </div>
+      {tenzies ? (
+        <h1>Congratulations</h1>
+      ) : (
+        <div>
+          <h1>Tenzies</h1>
+          <p>
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        </div>
+      )}
+      <div className="dice-container">{diceElements}</div>
+      <button onClick={tenzies ? newGame : rollDice} className="roll">
+        {tenzies ? "New Game" : "Roll Dice!"}
+      </button>
+      <div className="hallOfFame">
+        <ol>
+        <h3>The best Time</h3>
+        {bestTimes}
+      </ol>
+      <ol>
+        <h3>The Roll</h3>
+        {bestRolls}
+      </ol>
+        </div>
+    </main>
+
+  );
+}
+
+export default Tenzi;
